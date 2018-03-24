3d0199440bc853b7cb01b5dac5d5e11f897e7650
kashior <kashior@me.com>
2018-03-24 16:26:44 +0300

    First push

---
+++ cmake-build-debug/CMakeFiles/OS_ex1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_ex1.dir/main.cpp.o"
+  "OS_ex1.pdb"
+  "OS_ex1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS_ex1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
